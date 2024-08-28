622a381b98ea36a207bb0e40ec61a55d3d867a85
FoskyM <i@fosky.top>
2024-08-28 17:25:24 +0800

    feat: show monthly posts on user card

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,7 +1,43 @@
 import app from 'flarum/forum/app';
-import { override } from 'flarum/common/extend';
+import { override, extend } from 'flarum/common/extend';
+import User from 'flarum/common/models/User';
+import Model from 'flarum/common/Model';
+import UserCard from 'flarum/forum/components/UserCard';
+import icon from 'flarum/common/helpers/icon';
 
 app.initializers.add('foskym/flarum-sorts-for-user-directory', () => {
+  extend(UserCard.prototype, 'infoItems', function (items) {
+    const user = this.attrs.user;
+    if (!user) return;
+    if (!app.forum.attribute('foskym-sorts-for-user-directory.sort_by_monthly_posts')) return;
+    items.add(
+      'monthly-discussion-count',
+      <span className="UserCard-MonthlyDiscussionCount">
+        {[
+          icon('fas fa-comment'),
+          ' ',
+          app.translator.trans('foskym-sorts-for-user-directory.forum.user_card.monthly_discussions', {
+            count: user.data.attributes.monthlyDiscussionCount,
+          }),
+        ]}
+      </span>,
+      70
+    );
+    items.add(
+      'monthly-comment-count',
+      <span className="UserCard-MonthlyCommentCount">
+        {[
+          icon('fas fa-comments'),
+          ' ',
+          app.translator.trans('foskym-sorts-for-user-directory.forum.user_card.monthly_comments', {
+            count: user.data.attributes.monthlyCommentCount,
+          }),
+        ]}
+      </span>,
+      70
+    );
+  });
+
   if ('fof-user-directory' in flarum.extensions) {
     const UserDirectoryState = flarum.extensions['fof-user-directory']?.UserDirectoryState as any;
     override(UserDirectoryState.prototype, 'sortMap', function (original) {
